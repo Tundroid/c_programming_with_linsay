31f458197dd93372acb029912042008394f5c4d6
ndjakoulinsay <ndjakoulinsay@gmail.com>
2026-04-09 11:41:38 +0100

    my country documentation

---
+++ student_mgr/country.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+struct country
+{
+    int surface_area;
+    int population;
+    float birth_rate;
+    float death_rate;
+    char capital[20];
+    double currency_value;
+    char president[20];
+};
+int main()
+{
+return 0;
+}
