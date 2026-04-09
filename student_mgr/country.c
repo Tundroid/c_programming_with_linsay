59701644edb36d06f6470cacfa21aab140c75097
ndjakoulinsay <ndjakoulinsay@gmail.com>
2026-04-09 14:10:51 +0100

    country documentation

--- student_mgr/country.c
+++ student_mgr/country.c
@@ -7,10 +7,38 @@ struct country
     float birth_rate;
     float death_rate;
     char capital[20];
-    double currency_value;
+    char currency[4];
     char president[20];
 };
 int main()
 {
-return 0;
+    struct country c1[4];
+
+    int i;
+
+    for (i = 0; i < 4; i++)
+    {
+        printf("Enter name of country %d: ", i + 1);
+        scanf("%s", c1[i].capital);
+
+        printf("Enter surface area of country %d: ", i + 1);
+        scanf("%d", &c1[i].surface_area);
+
+        printf("Enter population of country %d: ", i + 1);
+        scanf("%d", &c1[i].population);
+
+        printf("Enter birth rate of country %d: ", i + 1);
+        scanf("%f", &c1[i].birth_rate);
+
+        printf("Enter death rate of country %d: ", i + 1);
+        scanf("%f", &c1[i].death_rate);
+
+        printf("Enter currency of country %d: ", i + 1);
+        scanf("%s", c1[i].currency);
+        
+        printf("Enter president of country %d: ", i + 1);
+        scanf("%s", c1[i].president);
+    }
+
+    return 0;
 }
